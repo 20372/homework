7be13e04f008b9d5d4c4e91bd1065b76491789ee
20372 <20372@wc.school.nz>
2024-03-12 14:18:34 +1300

    Setup Foreign Keys kinda

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "Homework"
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/home_page.html')
+def render_home_page():  # put application's code here
+    con = create_connection(DATABASE)
+    query = "SELECT * FROM Homework"
+    cur = con.cursor()
+    cur.execute(query)
+    students = cur.fetchall()
+    con.close()
+    return render_template('home_page.html', homework=students)
+
+
 
 
 if __name__ == '__main__':
